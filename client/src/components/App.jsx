77e3ee588b94ebfc8e3402cd8f5644d965f3bae2
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-11 18:19:31 -0400

    Continue installing necessary deps, create initial App.jsx, and render App to index.jsx.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+}
+
+export default App;

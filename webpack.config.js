8531a67e299fe53f54016c004925ff5675997914
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-11 18:08:18 -0400

    Complete data structure, install major dependencies, render initial html file, add necessary files for initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const OUT_DIR = path.join(__dirname, 'client', 'dist');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(SRC_DIR, 'index.js'),
+  output: {
+    path: OUT_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+};

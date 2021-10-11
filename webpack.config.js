aa12579e84076e798ffa38bd85340e4e726894d9
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-11 19:30:23 -0400

    Add babelrc file, and update webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -17,13 +17,13 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader'
       },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
+      // {
+      //   test: /\.css$/,
+      //   use: [
+      //     'style-loader',
+      //     'css-loader'
+      //   ]
+      // },
     ]
   },
   resolve: {

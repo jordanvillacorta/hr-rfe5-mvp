84546217a9c0ad25773e0126e032ac9442519fae
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-12 16:13:24 -0400

    Destructure props keys so it is easier to read.

--- client/src/components/MealTypes.jsx
+++ client/src/components/MealTypes.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const MealTypes = (props) => {
+const MealTypes = ({ mealType, handleChange, handleSubmit}) => {
 
   return (
     <div id="meal-selection">
       <h2>Meal Choices:</h2>
-      <form id="meal-selection-form" onSubmit={props.handleSubmit}>
+      <form id="meal-selection-form" onSubmit={handleSubmit}>
         <label>
           <span className="pick-your-meal">Pick your meal:</span>
-          <select id="meal-selection-button" value={props.mealType} onChange={props.handleChange}>
+          <select id="meal-selection-button" value={mealType} onChange={handleChange}>
             <option value="none">I'm feeling...</option>
             <option value="breakfast">Breakfast</option>
             <option value="lunch">Lunch</option>

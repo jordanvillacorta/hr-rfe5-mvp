fcc7f311cbeb291622cfd74aac9f8f248373dc10
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-12 17:06:17 -0400

    Import mealData into state, increment key id for mapping, render newly added SingleFood component to handle each individual object in mealData.

--- client/src/components/MealList.jsx
+++ client/src/components/MealList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SingleFood from './SingleFood.jsx';
 import mealData from '../mealData.json';
 
 class MealList extends React.Component {
@@ -9,23 +10,40 @@ class MealList extends React.Component {
       isLunch: false,
       isDinner: false,
       isDessert: false,
-      selectedFoods: []
+      selectedFoods: mealData,
+      keyIndex: 0
     }
+    this.getKey = this.getKey.bind(this);
   }
 
+  getKey() {
+    return this.state.keyIndex += 1;
+  }
 
-  // for (let i = 0; i < mealData.length; i++) {
-  //   if (mealData[i].mealType === 'breakfast') {
-  //     setFoodList(mealData[i]);
-  //   }
-  //   console.log(foodList);
-  // }
+  handleData = () => {
+    for (let i = 0; i < mealData.length; i++) {
+      if (mealData[i].mealType === 'breakfast') {
+        this.setState({
+          isBreakfast: true,
+          selectedFoods: mealData[i]
+        })
+      }
+      console.log(this.state.selectedFoods);
+    }
+  }
 
   render() {
     return (
       <div>
         <h2>Food List</h2>
-        {/* {breakfast && (mealData[0].mealType)} */}
+        {this.state.selectedFoods.map((singleFood) => {
+          return (
+            <SingleFood
+              key={this.getKey()}
+              singleFood={singleFood}
+            />
+          )
+        })}
         <button
           className="submit-btn"
           type="submit"

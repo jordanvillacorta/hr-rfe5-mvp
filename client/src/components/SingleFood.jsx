905c707ed50a779aff1550a1e82e1c50411ecb82
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-12 17:22:36 -0400

    Refactor functional to be class component, handle Meal Types and return individual food items.

--- client/src/components/SingleFood.jsx
+++ client/src/components/SingleFood.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
-const SingleFood = (props) => {
+class SingleFood extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isBreakfast: false,
+      isLunch: false,
+      isDinner: false,
+      isDessert: false
+    }
+    this.handleMealType = this.handleMealType.bind(this);
+  }
 
-  return (
-    <div id="single-food-item">
-      'SINGLE FOOD'
-    </div>);
+  handleMealType = () => {
+    if (mealType === 'breakfast') {
+      this.setState({
+        isBreakfast: true
+      });
+    }
+    if (mealType === 'lunch') {
+      this.setState({
+        isLunch: true
+      });
+    }
+    if (mealType === 'dinner') {
+      this.setState({
+        isDinner: true
+      });
+    }
+    if (mealType === 'dessert') {
+      this.setState({
+        isDessert: true
+      });
+    }
+  }
+
+  render() {
+    let singleFood = this.props.singleFood;
+    return (
+      <div id="single-food-item">
+        {singleFood}
+      </div>);
+  }
 }
 
 export default SingleFood;

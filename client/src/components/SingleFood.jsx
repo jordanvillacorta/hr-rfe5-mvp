511a8466e655de08202b614ccb9b89033c115c0a
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-13 03:02:26 -0400

    Clean up old code intended for conditional rendering. Now functioning as desired.

--- client/src/components/SingleFood.jsx
+++ client/src/components/SingleFood.jsx
@@ -5,61 +5,32 @@ class SingleFood extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allFoodTypes: this.props.mealType,
+      mealType: this.props.mealType,
       isBreakfast: false,
       isLunch: false,
       isDinner: false,
-      isDessert: false,
-      foodList: [],
+      isDessert: false
     };
-    this.handleMealType = this.handleMealType.bind(this);
   }
 
-  handleMealType = () => {
-    if (this.state.allFoodTypes === "breakfast") {
-      this.setState({
-        isBreakfast: true,
-      });
-    }
-    if (this.state.allFoodTypes === "lunch") {
-      this.setState({
-        isLunch: true,
-      });
-    }
-    if (this.state.allFoodTypes === "dinner") {
-      this.setState({
-        isDinner: true,
-      });
-    }
-    if (this.state.allFoodTypes === "dessert") {
-      this.setState({
-        isDessert: true,
-      });
-    }
-  };
-
   render() {
-    let singleFood = this.props.singleFood;
-    this.handleMealType();
     return (
       <div id="single-food-item">
         <div className="single-food-item-name">
           Name:
-          {/* {this.state.isBreakfast && ( */}
-            <div id="breakfast-food-item">{singleFood.name}</div>
-          {/* )} */}
+            <div id="breakfast-food-item">{this.props.singleFood.name}</div>
         </div>
         <div className="single-food-item-imageURL">
           Image:
-          {/* {this.state.isBreakfast && ( */}
           <div id="breakfast-food-item">
-            <img src={singleFood.imageURL} className="image-url" />
+            <img src={this.props.singleFood.imageURL} className="image-url" />
           </div>
-          {/* )} */}
         </div>
-        <button className="submit-btn" type="submit">
-          Select!
-        </button>
+        <form id="breakfast-food-select-btn" onClick={this.props.handleCurrentFood}>
+          <button className="submit-btn" type="submit" value={this.props.singleFood}>
+            Select!
+          </button>
+        </form>
       </div>
     );
   }

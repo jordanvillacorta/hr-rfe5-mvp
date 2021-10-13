3ab979a3c100e9ace0572589f1106cb973b034a4
Jordan Villacorta <jvillacortadla@gmail.com>
2021-10-13 02:59:37 -0400

    Major changes to state, added componentDidMount, added test function to check meal type, added handleCurrentFoodItem function, and am now mapping over filtered Foods!

--- client/src/components/MealList.jsx
+++ client/src/components/MealList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from "axios";
 import SingleFood from './SingleFood.jsx';
 import mealData from '../mealData.json';
 import '../../dist/styles.css';
@@ -7,43 +8,86 @@ class MealList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      mealType: this.props.mealType,
       isBreakfast: false,
       isLunch: false,
       isDinner: false,
       isDessert: false,
-      selectedFoods: mealData,
-      keyIndex: 0
+      selectedFoods: [],
+      filteredFoods: [],
+      keyIndex: 0,
+      currentFoodItem: '',
+      recipeList: [],
     }
     this.getKey = this.getKey.bind(this);
+    this.test = this.test.bind(this);
+    this.handleCurrentFoodItem = this.handleCurrentFoodItem.bind(this);
+  }
+
+  componentDidMount() {
+    axios.get('/meals')
+      .then((response) => {
+        this.setState({
+          selectedFoods: response.data
+        })
+        this.test();
+      })
+      .catch(err => {
+        console.log('Error with rendering page:', err);
+      })
+  }
+
+  test() {
+    let breakfastList = [];
+    let selectedFoods = this.state.selectedFoods;
+    for (let i = 0; i < selectedFoods.length; i++) {
+      if (selectedFoods[i].mealType === 'breakfast') {
+        breakfastList.push(selectedFoods[i]);
+      }
+      console.log('list', breakfastList);
+      this.setState({
+        isBreakfast: true,
+        filteredFoods: breakfastList
+      });
+    }
   }
 
   getKey() {
     return this.state.keyIndex += 1;
   }
 
-  // handleData = () => {
-  //   for (let i = 0; i < mealData.length; i++) {
-  //     if (mealData[i].mealType === 'breakfast') {
-  //       this.setState({
-  //         isBreakfast: true
-  //       })
-  //     }
-  //     console.log(this.state.selectedFoods);
-  //   }
-  // }
+  handleCurrentFoodItem = (e) => {
+    e.preventDefault();
+    // console.log(e.target, 'clicked!')
+    this.setState({
+      currentFoodItem: e.target.value
+    })
+  }
 
   render() {
     return (
       <div id="food-list-container">
         <h2>Food List</h2>
-        {this.state.selectedFoods.map((singleFood) => {
+        {this.state.filteredFoods.map((singleFood) => {
           return (
             <SingleFood
               key={this.getKey()}
+              mealType={this.state.mealType}
               singleFood={singleFood}
+              handleCurrentFood={this.handleCurrentFoodItem}
             />
           )
         })}
+        {/* {this.state.selectedFoods.map((singleFood) => {
+          return (
+            <SingleFood
+              key={this.getKey()}
+              mealType={this.state.mealType}
+              singleFood={singleFood}
+              handleCurrentFood={this.handleCurrentFoodItem}
+            />
+          )
+        })} */}
       </div>
     );
   }

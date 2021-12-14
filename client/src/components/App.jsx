ea768c6843efb011f1cc6727e58eb81a66e00121
Jordan Villacorta <jvillacortadla@gmail.com>
2021-12-13 17:52:14 -0700

    Take return statement out of render scope and begin switching classical react syntax to function (with useState).

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -99,57 +99,54 @@ import "../../dist/styles.css";
     return this.state.mealId += 1;
   }
 
-  // render() {
-  //   return (
-  //     <div id="meal-planner-wrapper">
-  //       <div id="meal-planner-container">
-  //         <div id="meal-planner-types">
-  //           <MealTypes
-  //             mealType={this.state.mealType}
-  //             handleChange={this.handleMealTypeChange}
-  //             handleSubmit={this.handleMealTypeSubmit}
-  //           />
-  //           {(this.state.submitted && this.state.mealType !== '' && this.state.mealType !== 'none') && (
-  //             <div className="meal-planner-choice">
-  //               {this.state.mealType} is a great choice!
-  //             </div>
-  //           )}
-  //         </div>
-  //         {(this.state.didMount) && (
-  //           <div id="meal-planner-meal-list">
-  //             <MealList
-  //               mealId={this.getMealId()}
-  //               didMount={this.state.didMount}
-  //               foodsList={this.state.selectedFoods}
-  //               mealType={this.state.mealType}
-  //               handleCurrentFood={this.handleCurrentFoodItem}
-  //               // handleRecipeSubmit={this.handleRecipeSubmit}
-  //             />
-  //           </div>
-  //         )}
-  //         {(this.state.didMount) && (
-  //         <div id="meal-planner-recipe-list">
-  //           <RecipeList recipeList={this.state.recipeList} />
-  //         </div>
-  //         )}
-  //       </div>
-  //       <div id="save-meal-plan">
-  //         <button className="save-meal-plan-btn" type="submit" onClick={this.handleSaveMealClick} >
-  //           SAVE MEAL PLAN
-  //         </button>
-  //       </div>
-  //       <br></br>
-  //       <div>
-  //         {this.state.saveMeal && (
-  //           <div id="saved-meal-plan">
-  //             You saved your meal plan!
-  //           </div>
-  //         )}
-  //       </div>
-  //     </div>
-  //   );
-  // }
-// }
+  return (
+    <div id="meal-planner-wrapper">
+      <div id="meal-planner-container">
+        <div id="meal-planner-types">
+          <MealTypes
+            mealType={this.state.mealType}
+            handleChange={this.handleMealTypeChange}
+            handleSubmit={this.handleMealTypeSubmit}
+          />
+          {(this.state.submitted && this.state.mealType !== '' && this.state.mealType !== 'none') && (
+            <div className="meal-planner-choice">
+              {this.state.mealType} is a great choice!
+            </div>
+          )}
+        </div>
+        {(this.state.didMount) && (
+          <div id="meal-planner-meal-list">
+            <MealList
+              mealId={this.getMealId()}
+              didMount={this.state.didMount}
+              foodsList={this.state.selectedFoods}
+              mealType={this.state.mealType}
+              handleCurrentFood={this.handleCurrentFoodItem}
+              // handleRecipeSubmit={this.handleRecipeSubmit}
+            />
+          </div>
+        )}
+        {(this.state.didMount) && (
+        <div id="meal-planner-recipe-list">
+          <RecipeList recipeList={this.state.recipeList} />
+        </div>
+        )}
+      </div>
+      <div id="save-meal-plan">
+        <button className="save-meal-plan-btn" type="submit" onClick={this.handleSaveMealClick} >
+          SAVE MEAL PLAN
+        </button>
+      </div>
+      <br></br>
+      <div>
+        {this.state.saveMeal && (
+          <div id="saved-meal-plan">
+            You saved your meal plan!
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;

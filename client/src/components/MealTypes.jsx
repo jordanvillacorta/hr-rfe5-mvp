9902aa71f3e2fc4e611df81f045d0f6fea7b1d56
Jordan Villacorta <jvillacortadla@gmail.com>
2021-12-18 01:22:44 -0700

    Add helper functions to invoke passed down handlers for submit and change.

--- client/src/components/MealTypes.jsx
+++ client/src/components/MealTypes.jsx
@@ -2,16 +2,24 @@ import React, { useState, useEffect } from "react";
 import '../../dist/styles.css';
 
 const MealTypes = ({ mealType, handleChange, handleSubmit }) => {
+  const onChangeHandle = () => {
+    handleChange();
+  }
+
+  const onSubmitHandle = () => {
+    handleSubmit();
+  }
+
   return (
     <div id="meal-selection">
       <h2 className="col-headers">Meal Choices:</h2>
-      <form id="meal-selection-form" onSubmit={handleSubmit()}>
+      <form id="meal-selection-form" onSubmit={onSubmitHandle}>
         <label>
           <span className="pick-your-meal">Pick your meal: </span>
           <select
             id="meal-selection-button"
-            value={mealType()}
-            onChange={handleChange()}
+            value={mealType}
+            onChange={onChangeHandle}
           >
             <option className="select-option" value="none">I'm feeling...</option>
             <option className="select-option" value="Breakfast">Breakfast</option>

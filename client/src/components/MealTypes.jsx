66762fc7e9450a1d901d3d74f5f5fe08475f0eeb
Jordan Villacorta <jvillacortadla@gmail.com>
2021-12-17 23:44:28 -0700

    Test if invoking passed down helper functions works properly.

--- client/src/components/MealTypes.jsx
+++ client/src/components/MealTypes.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../../dist/styles.css';
 
 const MealTypes = ({ mealType, handleChange, handleSubmit }) => {
   return (
     <div id="meal-selection">
       <h2 className="col-headers">Meal Choices:</h2>
-      <form id="meal-selection-form" onSubmit={handleSubmit}>
+      <form id="meal-selection-form" onSubmit={handleSubmit()}>
         <label>
           <span className="pick-your-meal">Pick your meal: </span>
           <select
             id="meal-selection-button"
-            value={mealType}
-            onChange={handleChange}
+            value={mealType()}
+            onChange={handleChange()}
           >
             <option className="select-option" value="none">I'm feeling...</option>
             <option className="select-option" value="Breakfast">Breakfast</option>

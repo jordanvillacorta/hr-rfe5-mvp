f8cfd6fbec1c9d6cdbad1aa4d1d8edd96ffa534a
Jordan Villacorta <jvillacortadla@gmail.com>
2021-12-20 21:44:51 -0700

    Refactor component from classical to functional. Begin working on return statement.

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -2,22 +2,22 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import SingleRecipe from "./SingleRecipe.jsx";
 
-// const RecipeList2 = ({ recipeList }) => {
-//   const [foodList, setFoodList] = useState([]);
-//   const [formattedRecipes, setFormattedRecipes] = useState([]);
-//   const [didListMount, setDidListMount] = useState(false);
+const RecipeList = ({ recipeList }) => {
+  const [foodList, setFoodList] = useState([]);
+  const [formattedRecipes, setFormattedRecipes] = useState([]);
+  const [didListMount, setDidListMount] = useState(false);
 
-//   useEffect(() => {
-//     axios.get('/meals')
-//       .then((response) => {
-//         setFoodList(response.data);
-//         setDidListMount(true);
-//         formatData();
-//       })
-//       .catch((err) => {
-//         console.error("Error with rendering page:", err);
-//       });
-//   }, []);
+  useEffect(() => {
+    axios.get('/meals')
+      .then((response) => {
+        setFoodList(response.data);
+        setDidListMount(true);
+        formatData();
+      })
+      .catch((err) => {
+        console.error("Error with rendering page:", err);
+      });
+  }, []);
 
   const formatData = () => {
     // let foodList = this.state.foodList;
@@ -33,51 +33,51 @@ import SingleRecipe from "./SingleRecipe.jsx";
       //   formattedRecipes: names
       // })
   }
-// }
+}
 
 
-class RecipeList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      foodList: [],
-      formattedRecipes: [],
-      didMount: false
-    };
-    this.formatData = this.formatData.bind(this);
-    // this.test = this.test.bind(this);
-  }
+// class RecipeList extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       foodList: [],
+//       formattedRecipes: [],
+//       didMount: false
+//     };
+//     this.formatData = this.formatData.bind(this);
+//     // this.test = this.test.bind(this);
+//   }
 
-  componentDidMount() {
-    axios.get('/meals')
-      .then((response) => {
-        this.setState({
-          foodList: response.data,
-          didMount: true,
-        });
-        this.formatData();
-        // this.test();
-      })
-      .catch((err) => {
-        console.log("Error with rendering page:", err);
-      });
-  }
+//   componentDidMount() {
+//     axios.get('/meals')
+//       .then((response) => {
+//         this.setState({
+//           foodList: response.data,
+//           didMount: true,
+//         });
+//         this.formatData();
+//         // this.test();
+//       })
+//       .catch((err) => {
+//         console.log("Error with rendering page:", err);
+//       });
+//   }
 
-  formatData = () => {
-    let foodList = this.state.foodList;
-    let names = foodList.map((singleFood) => {
-      return (
-        {
-          name: singleFood.name,
-          ingredients: singleFood.ingredients,
-          steps: singleFood.steps
-        }
-        )
-      })
-      this.setState({
-        formattedRecipes: names
-      })
-  }
+//   formatData = () => {
+//     let foodList = this.state.foodList;
+//     let names = foodList.map((singleFood) => {
+//       return (
+//         {
+//           name: singleFood.name,
+//           ingredients: singleFood.ingredients,
+//           steps: singleFood.steps
+//         }
+//         )
+//       })
+//       this.setState({
+//         formattedRecipes: names
+//       })
+//   }
 
   // test = () => {
   //   let recipeList = this.props.recipeList;
@@ -91,25 +91,25 @@ class RecipeList extends React.Component {
   //   }
   // }
 
-  render() {
-    let formattedRecipes = this.state.formattedRecipes;
-    let recipeList = this.props.recipeList;
-    // console.log('recipeList', recipeList);
-    return (
-      <div id="recipe-list-container">
-        <h2 className="col-headers">Recipe List</h2>
-        {recipeList.map((singleRecipe) => {
-          console.log('singleRecipe', singleRecipe)
-          return (
-            <SingleRecipe
-              singleRecipe={singleRecipe}
-              // formattedRecipes={formattedRecipes}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+  // render() {
+  //   let formattedRecipes = this.state.formattedRecipes;
+  //   let recipeList = this.props.recipeList;
+  //   // console.log('recipeList', recipeList);
+  //   return (
+  //     <div id="recipe-list-container">
+  //       <h2 className="col-headers">Recipe List</h2>
+  //       {recipeList.map((singleRecipe) => {
+  //         console.log('singleRecipe', singleRecipe)
+  //         return (
+  //           <SingleRecipe
+  //             singleRecipe={singleRecipe}
+  //             // formattedRecipes={formattedRecipes}
+  //           />
+  //         );
+  //       })}
+  //     </div>
+  //   );
+  // }
+// }
 
 export default RecipeList;

6567f41908e0e8d36d3dde42b2e11b59fb5d36f5
Jordan Villacorta <jvillacortadla@gmail.com>
2022-02-03 22:30:06 -0700

    Format with Prettier.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,27 +5,28 @@ import MealList from "./MealList.jsx";
 import RecipeList from "./RecipeList.jsx";
 import "../../dist/styles.css";
 
-  const App = () => {
-    const [mealId, setMealId] = useState(0);
-    const [mealType, setMealType] = useState('');
-    const [mealList, setMealList] = useState([]);
-    const [currentFoodItem, setCurrentFoodItem] = useState('');
-    const [recipeList, setRecipeList] = useState([]);
-    const [submitted, isSubmitted] = useState(false);
-    const [selectedFoods, setSelectedFoods] = useState([]);
-    const [didMount, setDidMount] = useState(false);
-    const [saveMeal, setSaveMeal] = useState(false);
+const App = () => {
+  const [mealId, setMealId] = useState(0);
+  const [mealType, setMealType] = useState("");
+  const [mealList, setMealList] = useState([]);
+  const [currentFoodItem, setCurrentFoodItem] = useState("");
+  const [recipeList, setRecipeList] = useState([]);
+  const [submitted, isSubmitted] = useState(false);
+  const [selectedFoods, setSelectedFoods] = useState([]);
+  const [didMount, setDidMount] = useState(false);
+  const [saveMeal, setSaveMeal] = useState(false);
 
-    useEffect(() => {
-      axios.get('/meals')
-        .then((response) => {
-          setSelectedFoods(response.data);
-          setDidMount(true);
-        })
-        .catch((err) => {
-          console.error("Error with rendering page:", err);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("/meals")
+      .then((response) => {
+        setSelectedFoods(response.data);
+        setDidMount(true);
+      })
+      .catch((err) => {
+        console.error("Error with rendering page:", err);
+      });
+  }, []);
 
   const handleMealTypeChange = (e) => {
     let newMealType = e.target.value;
@@ -42,16 +43,16 @@ import "../../dist/styles.css";
     let newFoodItem = e.target.value;
     setCurrentFoodItem([newFoodItem]);
     setRecipeList(recipeList.concat([newFoodItem]));
-  }
+  };
 
   const handleSaveMealClick = (e) => {
     e.preventDefault();
     setSaveMeal(true);
-  }
+  };
 
   const getMealId = () => {
     setMealId(mealId + 1);
-  }
+  };
 
   return (
     <div id="meal-planner-wrapper">
@@ -63,7 +64,7 @@ import "../../dist/styles.css";
               handleChange={handleMealTypeChange}
               handleSubmit={handleMealTypeSubmit}
             />
-            {(submitted && mealType !== '' && mealType !== 'none') && (
+            {submitted && mealType !== "" && mealType !== "none" && (
               <div className="meal-planner-choice">
                 {mealType} is a great choice!
               </div>
@@ -82,28 +83,26 @@ import "../../dist/styles.css";
           </div>
         )}
         {didMount && (
-        <div id="meal-planner-recipe-list">
-          <RecipeList
-            recipeList={recipeList}
-          />
-        </div>
+          <div id="meal-planner-recipe-list">
+            <RecipeList recipeList={recipeList} />
+          </div>
         )}
       </div>
       <div id="save-meal-plan">
-        <button className="save-meal-plan-btn" type="submit" onClick={handleSaveMealClick} >
+        <button
+          className="save-meal-plan-btn"
+          type="submit"
+          onClick={handleSaveMealClick}
+        >
           SAVE MEAL PLAN
         </button>
       </div>
       <br></br>
       <div>
-      {saveMeal && (
-        <div id="saved-meal-plan">
-          You saved your meal plan!
-        </div>
-      )}
+        {saveMeal && <div id="saved-meal-plan">You saved your meal plan!</div>}
       </div>
     </div>
   );
-}
+};
 
 export default App;
